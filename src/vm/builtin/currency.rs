f1a6547150f8a5299f07efb2450a2ebadef52272
Karim Agha <karim.dev@gmail.com>
2022-02-11 12:07:20 +0000

    currency module implementation complete

--- src/vm/builtin/currency.rs
+++ src/vm/builtin/currency.rs
@@ -7,7 +7,7 @@ use {
   crate::{
     primitives::Pubkey,
     vm::{
-      contract::{self, ContractError, Environment},
+      contract::{self, AccountView, ContractError, Environment},
       transaction::SignatureError,
     },
   },
@@ -33,6 +33,7 @@ pub struct Mint {
   pub supply: u64,
 
   /// The number of base 10 digits to the right of the decimal place.
+  /// For NFTs this is always 0.
   pub decimals: u8,
 
   /// The long version of the token name
@@ -123,7 +124,8 @@ pub enum Instruction {
   ///  0. [d-r--] The mint address
   ///  1. [---s] The sender wallet owner address as signer
   ///  2. [drw-] The sender token address (generated through
-  /// Currency.derive([mint, wallet]))  2. [drw-] The recipient token address
+  /// Currency.derive([mint, wallet]))               
+  ///  3. [drw-] The recipient token address
   /// (generated through Currency.derive([mint, wallet]))
   Transfer(u64),
 
@@ -135,8 +137,7 @@ pub enum Instruction {
   /// Accounts expected by this instruction:
   ///  0. [drw-] The mint address
   ///  1. [---s] The wallet owner address as signer
-  ///  2. [drw-] The token address (generated through Currency.derive([mint,
-  /// wallet]))
+  ///  2. [drw-] The coin account address
   Burn(u64),
 
   /// Changes the mint authority of the token.
@@ -244,6 +245,12 @@ fn process_create(
     // generate logs
     contract::Output::LogEntry("action".into(), "mint-created".into()),
     contract::Output::LogEntry("address".into(), addr.to_string()),
+    contract::Output::LogEntry("name".into(), spec.name.unwrap_or_default()),
+    contract::Output::LogEntry(
+      "symbol".into(),
+      spec.symbol.unwrap_or_default(),
+    ),
+    contract::Output::LogEntry("decumals".into(), decimals.to_string()),
   ])
 }
 
@@ -254,36 +261,17 @@ fn process_mint(env: &Environment, amount: u64) -> contract::Result {
     return Err(ContractError::InvalidInputAccounts);
   }
 
+  //  0. [d-rw] The mint address
+  //  1. [---s] The mint authority as signer
+  //  2. [----] The recipient wallet owner address
+  //  3. [d-rw] The recipient address (coin address)
   let (mint_addr, mint_data) = &env.accounts[0];
   let (authority, authority_acc) = &env.accounts[1];
   let (wallet_addr, _) = &env.accounts[2];
   let (coin_addr, coin_acc) = &env.accounts[3];
 
-  // the mint account must be a derived account
-  if mint_addr.has_private_key() {
-    return Err(ContractError::InvalidInputAccounts);
-  }
-
-  // the mint account must be owned by the currency contract
-  match mint_data.owner {
-    Some(ref owner) => {
-      if owner != &env.address {
-        return Err(ContractError::InvalidAccountOwner);
-      }
-    }
-    None => {
-      return Err(ContractError::InvalidAccountOwner);
-    }
-  }
-
-  // get the mint account data
-  let mut mint: Mint = match mint_data.data {
-    Some(ref data) => BorshDeserialize::try_from_slice(data)
-      .map_err(|_| ContractError::InvalidInputAccounts)?,
-    None => {
-      return Err(ContractError::InvalidInputAccounts);
-    }
-  };
+  // validate and read coin mint data
+  let mut mint = read_coin_mint(mint_addr, mint_data, env)?;
 
   // make sure that the caller is authorized to mint new tokens
   if let Some(ref mint_authority) = mint.authority {
@@ -308,55 +296,36 @@ fn process_mint(env: &Environment, amount: u64) -> contract::Result {
     )));
   }
 
-  // verify that the coin account is the exected
-  // currency account for the given wallet
-  if coin_addr != &env.address.derive(&[mint_addr, wallet_addr]) {
-    return Err(ContractError::InvalidInputAccounts);
-  }
-
-  // verify that it is owned by the currency contract
-  if let Some(ref owner) = coin_acc.owner {
-    if owner != &env.address {
-      return Err(ContractError::InvalidAccountOwner);
-    }
-  }
-
+  // logs for explorers and dApps
   let mut outputs = vec![
     contract::Output::LogEntry("action".into(), "mint-coins".into()),
     contract::Output::LogEntry("to".into(), wallet_addr.to_string()),
   ];
 
-  // all checks passed, now the coin account either
-  // has to be created because this is the first time
-  // this wallet is used for this coin, or modified
+  // all checks passed, now the coin account either has to be created because
+  // this is the first time this wallet is used for this coin, or modified
   // by increasing its balance of this coin
-  if let Some(ref data) = coin_acc.data {
-    // already exists, read its contents
-    let mut coin: CoinAccount = BorshDeserialize::try_from_slice(data)
-      .map_err(|_| ContractError::InvalidInputAccounts)?;
-
-    // make sure that the coin account is for the minted coin type
-    if mint_addr != &coin.mint {
-      return Err(ContractError::InvalidInputAccounts);
+  match read_coin_account(coin_addr, coin_acc, mint_addr, wallet_addr, env)? {
+    Some(mut coin) => {
+      coin.balance = coin.balance.saturating_add(amount);
+      outputs.push(contract::Output::ModifyAccountData(
+        *coin_addr,
+        Some(coin.try_to_vec()?),
+      ))
     }
-
-    coin.balance = coin.balance.saturating_add(amount);
-    outputs.push(contract::Output::ModifyAccountData(
-      *coin_addr,
-      Some(coin.try_to_vec()?),
-    ))
-  } else {
-    // never used before for this coin, create
-    let coin = CoinAccount {
-      mint: *mint_addr,
-      balance: amount,
-      owner: *wallet_addr,
-    };
-    outputs.push(contract::Output::CreateOwnedAccount(
-      *coin_addr,
-      Some(coin.try_to_vec()?),
-    ));
-  }
+    None => {
+      // never used before for this coin, create
+      let coin = CoinAccount {
+        mint: *mint_addr,
+        balance: amount,
+        owner: *wallet_addr,
+      };
+      outputs.push(contract::Output::CreateOwnedAccount(
+        *coin_addr,
+        Some(coin.try_to_vec()?),
+      ));
+    }
+  };
 
   // update the global supply value
   mint.supply = mint.supply.saturating_add(amount);
@@ -368,17 +337,305 @@ fn process_mint(env: &Environment, amount: u64) -> contract::Result {
   Ok(outputs)
 }
 
-fn process_transfer(_env: &Environment, _amount: u64) -> contract::Result {
-  todo!();
+fn process_transfer(env: &Environment, amount: u64) -> contract::Result {
+  if env.accounts.len() != 5 {
+    return Err(ContractError::InvalidInputAccounts);
+  }
+
+  //  0. [d-r--] The mint address
+  //  1. [---s] The sender wallet owner address as signer
+  //  2. [drw-] The sender coin address
+  //  3. [----] The recipient wallet owner address
+  //  4. [drw-] The recipient token address
+  let (mint_addr, mint_acc) = &env.accounts[0];
+  let (sender_wallet_addr, sender_wallet_acc) = &env.accounts[1];
+  let (sender_coin_addr, sender_coin_acc) = &env.accounts[2];
+  let (recipient_wallet_addr, _) = &env.accounts[3];
+  let (recipient_coin_addr, recipient_coin_acc) = &env.accounts[4];
+
+  // make sure that the mint address points to a valid coin mint.
+  let mint = read_coin_mint(mint_addr, mint_acc, env)?;
+
+  // validate and read coin mint data, this also validates
+  // the validity of the derived coin address.
+  if let Some(mut sender_coin) = read_coin_account(
+    sender_coin_addr,
+    sender_coin_acc,
+    mint_addr,
+    sender_wallet_addr,
+    env,
+  )? {
+    // make sure that the transafer is authorized
+    // by the coin wallet account owner
+    if !sender_wallet_acc.signer {
+      return Err(ContractError::SignatureError(
+        SignatureError::MissingSigners,
+      ));
+    }
+
+    // make sure that the sender has enough balance
+    if sender_coin.balance < amount {
+      return Err(ContractError::Other(std::io::Error::new(
+        std::io::ErrorKind::Interrupted,
+        "Not enough balance in sender's account",
+      )));
+    }
+
+    // all checks passed, first debit the sender account
+    sender_coin.balance = sender_coin.balance.saturating_sub(amount);
+
+    let mut outputs = vec![
+      // logs for explorers and dApps
+      contract::Output::LogEntry("action".into(), "transfer".into()),
+      contract::Output::LogEntry("from".into(), sender_wallet_addr.to_string()),
+      contract::Output::LogEntry("coin.address".into(), mint_addr.to_string()),
+      contract::Output::LogEntry(
+        "coin.name".into(),
+        mint.name.unwrap_or_default(),
+      ),
+      contract::Output::LogEntry(
+        "coin.symbol".into(),
+        mint.symbol.unwrap_or_default(),
+      ),
+      contract::Output::LogEntry(
+        "to".into(),
+        recipient_wallet_addr.to_string(),
+      ),
+      contract::Output::LogEntry("amount".into(), amount.to_string()),
+      // store updated debited sender account
+      contract::Output::ModifyAccountData(
+        *sender_coin_addr,
+        Some(sender_coin.try_to_vec()?),
+      ),
+    ];
+
+    match read_coin_account(
+      recipient_coin_addr,
+      recipient_coin_acc,
+      mint_addr,
+      recipient_wallet_addr,
+      env,
+    )? {
+      // the recipient already has a coin account for this coin type
+      Some(mut recipient_coin) => {
+        recipient_coin.balance = recipient_coin.balance.saturating_add(amount);
+        outputs.push(contract::Output::ModifyAccountData(
+          *recipient_coin_addr,
+          Some(recipient_coin.try_to_vec()?),
+        ));
+      }
+      None => {
+        // this is the first time a coin of this type is
+        // transferred to this wallet, create a coin account
+        let coin = CoinAccount {
+          mint: *mint_addr,
+          balance: amount,
+          owner: *recipient_wallet_addr,
+        };
+
+        // create coin account for recipient wallet
+        outputs.push(contract::Output::CreateOwnedAccount(
+          *recipient_coin_addr,
+          Some(coin.try_to_vec()?),
+        ));
+      }
+    }
+
+    // success, coins transfer completed
+    Ok(outputs)
+  } else {
+    Err(ContractError::AccountDoesNotExist)
+  }
 }
 
-fn process_burn(_env: &Environment, _amount: u64) -> contract::Result {
-  todo!();
+fn process_burn(env: &Environment, amount: u64) -> contract::Result {
+  if env.accounts.len() != 3 {
+    return Err(ContractError::InvalidInputAccounts);
+  }
+
+  // Accounts expected by this instruction:
+  //  0. [drw-] The mint address
+  //  1. [---s] The wallet owner address as signer
+  //  2. [drw-] The coin account address
+  let (mint_addr, mint_acc) = &env.accounts[0];
+  let (wallet_addr, wallet_acc) = &env.accounts[1];
+  let (coin_addr, coin_acc) = &env.accounts[2];
+
+  let mut mint = read_coin_mint(mint_addr, mint_acc, env)?;
+  if let Some(mut coin) =
+    read_coin_account(coin_addr, coin_acc, mint_addr, wallet_addr, env)?
+  {
+    // make sure the owning wallet is authorizing this burn
+    if !wallet_acc.signer {
+      return Err(ContractError::SignatureError(
+        SignatureError::MissingSigners,
+      ));
+    }
+
+    // make sure the wallet coin account owns enough coins to burn
+    if coin.balance < amount {
+      return Err(ContractError::Other(std::io::Error::new(
+        std::io::ErrorKind::Interrupted,
+        "Not enough balance in sender's account",
+      )));
+    }
+
+    // all good, decrement balances on the account and the total coin supply
+    mint.supply = mint.supply.saturating_sub(amount);
+    coin.balance = coin.balance.saturating_sub(amount);
+
+    let coin_name = mint.name.clone().unwrap_or_default();
+    let coin_symbol = mint.symbol.clone().unwrap_or_default();
+
+    Ok(vec![
+      // logs for explorers and dApps
+      contract::Output::LogEntry("action".into(), "burn".into()),
+      contract::Output::LogEntry("from".into(), wallet_addr.to_string()),
+      contract::Output::LogEntry("amount".into(), amount.to_string()),
+      contract::Output::LogEntry("coin.address".into(), mint_addr.to_string()),
+      contract::Output::LogEntry("coin.name".into(), coin_name),
+      contract::Output::LogEntry("coin.symbol".into(), coin_symbol),
+      // store updated accounts
+      contract::Output::ModifyAccountData(*mint_addr, Some(mint.try_to_vec()?)),
+      contract::Output::ModifyAccountData(*coin_addr, Some(coin.try_to_vec()?)),
+    ])
+  } else {
+    Err(ContractError::AccountDoesNotExist)
+  }
 }
 
 fn process_set_authority(
-  _env: &Environment,
-  _authority: Option<Pubkey>,
+  env: &Environment,
+  authority: Option<Pubkey>,
 ) -> contract::Result {
-  todo!();
+  if env.accounts.len() != 2 {
+    return Err(ContractError::InvalidInputAccounts);
+  }
+
+  // Accounts expected by this instruction:
+  //  0. [drw-] The mint address
+  //  1. [---s] The the current authority wallet as signer
+  let (mint_addr, mint_acc) = &env.accounts[0];
+  let (current_auth_addr, current_auth_acc) = &env.accounts[1];
+
+  let mut mint = read_coin_mint(mint_addr, mint_acc, env)?;
+
+  if let Some(ref existing_authority) = mint.authority {
+    if existing_authority != current_auth_addr {
+      return Err(ContractError::Other(std::io::Error::new(
+        std::io::ErrorKind::PermissionDenied,
+        "account not authorized to change mint authority",
+      )));
+    }
+
+    // make sure that the current authority is authorizing this change
+    if !current_auth_acc.signer {
+      return Err(ContractError::SignatureError(
+        SignatureError::MissingSigners,
+      ));
+    }
+
+    // update authority on mint metadata
+    mint.authority = authority;
+
+    let coin_name = mint.name.clone().unwrap_or_default();
+    let coin_symbol = mint.symbol.clone().unwrap_or_default();
+
+    Ok(vec![
+      // logs for dApps and explorers
+      contract::Output::LogEntry("action".into(), "set-authority".into()),
+      contract::Output::LogEntry("coin.address".into(), mint_addr.to_string()),
+      contract::Output::LogEntry("coin.name".into(), coin_name),
+      contract::Output::LogEntry("coin.symbol".into(), coin_symbol),
+      contract::Output::LogEntry(
+        "to".into(),
+        authority.map(|a| a.to_string()).unwrap_or_default(),
+      ),
+      // update mint storage value
+      contract::Output::ModifyAccountData(*mint_addr, Some(mint.try_to_vec()?)),
+    ])
+  } else {
+    Err(ContractError::Other(std::io::Error::new(
+      std::io::ErrorKind::PermissionDenied,
+      "Authority has been removed for this coin",
+    )))
+  }
+}
+
+/// Verifies that the given account is a valid
+/// coin mint account and returns its deserialized
+/// representation.
+fn read_coin_mint(
+  addr: &Pubkey,
+  acc: &AccountView,
+  env: &Environment,
+) -> Result<Mint, ContractError> {
+  // the mint account must be a derived account
+  if addr.has_private_key() {
+    return Err(ContractError::InvalidInputAccounts);
+  }
+
+  // the mint account must be owned by the currency contract
+  match acc.owner {
+    Some(ref owner) => {
+      if owner != &env.address {
+        return Err(ContractError::InvalidAccountOwner);
+      }
+    }
+    None => {
+      return Err(ContractError::InvalidAccountOwner);
+    }
+  }
+
+  // get the mint account data
+  match acc.data {
+    Some(ref data) => BorshDeserialize::try_from_slice(data)
+      .map_err(|_| ContractError::InvalidInputAccounts),
+    None => Err(ContractError::InvalidInputAccounts),
+  }
+}
+
+/// Verifies that the coin account is of the same type
+/// as the specified mint and returns the account data
+/// if the coin account exists, otherwise if the wallet
+/// was never used before for this coin, None is returned
+fn read_coin_account(
+  coin_addr: &Pubkey,
+  coin_acc: &AccountView,
+  mint_addr: &Pubkey,
+  wallet_addr: &Pubkey,
+  env: &Environment,
+) -> Result<Option<CoinAccount>, ContractError> {
+  // verify that the coin account is the expected
+  // currency account for the given wallet
+  if coin_addr != &env.address.derive(&[mint_addr, wallet_addr]) {
+    return Err(ContractError::InvalidInputAccounts);
+  }
+
+  // verify that it is owned by the currency contract
+  // if the account already exists
+  if let Some(ref owner) = coin_acc.owner {
+    if owner != &env.address {
+      return Err(ContractError::InvalidAccountOwner);
+    }
+  }
+
+  if let Some(ref data) = coin_acc.data {
+    // already exists, read its contents
+    let coin: CoinAccount = BorshDeserialize::try_from_slice(data)
+      .map_err(|_| ContractError::InvalidInputAccounts)?;
+
+    // make sure that the coin account is for the minted coin type
+    if mint_addr != &coin.mint {
+      return Err(ContractError::InvalidInputAccounts);
+    }
+
+    if wallet_addr != &coin.owner {
+      return Err(ContractError::InvalidAccountOwner);
+    }
+
+    Ok(Some(coin))
+  } else {
+    Ok(None)
+  }
 }
