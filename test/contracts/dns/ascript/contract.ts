dcd752df2901675537a6066a05d699449d68d5b5
Karim Agha <karim.dev@gmail.com>
2022-04-04 05:58:26 +0000

    VM: Working on contract ABI part 2 of N

--- test/contracts/dns/ascript/contract.ts
+++ test/contracts/dns/ascript/contract.ts
@@ -7,9 +7,26 @@ class InputAccount {
 class Environment {
   address: Pubkey;
   accounts: Array<InputAccount>
+
+  constructor() {
+    this.address = new Pubkey();
+    this.accounts = new Array<InputAccount>(0);
+  }
 }
 class Output { }
 
-export function contract(env: Environment, b: Uint8Array): Array<Output> | Error {
-  return new Error("Not implemented yet");
+function createOutput(output: Output): u32 {
+  return changetype<u32>(heap.alloc(sizeof<Output>()));
+}
+
+export function allocate(size: u32): u32 {
+  return changetype<u32>(heap.alloc(changetype<usize>(size)));
+}
+
+export function environment(ptr: u32): Environment {
+  return new Environment();
+}
+
+export function contract(env: Environment, params: u8[]): u32 {
+  return createOutput(new Output());
 }
--- test/contracts/dns/rust/src/lib.rs
+++ test/contracts/dns/rust/src/lib.rs
@@ -1,4 +1,31 @@
+#[repr(C)]
+struct Region {
+  len: u32,
+}
+
+pub struct Environemnt {
+  val: u32,
+}
+
 #[no_mangle]
-pub extern "C" fn contract(env: i32) -> i32 {
-  env + 18
+pub unsafe extern "C" fn allocate(size: u32) -> *mut u8 {
+  let mut buf = Vec::with_capacity(size as usize);
+  let ptr = buf.as_mut_ptr();
+  core::mem::forget(buf);
+  ptr
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn environment(_ptr: *const u8) -> *const Environemnt {
+  let out = Environemnt { val: 18 };
+  let outptr = &out as *const Environemnt;
+  core::mem::forget(out);
+  outptr
+}
+
+#[no_mangle]
+pub extern "C" fn contract(env: &Environemnt, params: *const u8) -> u32 {
+  let region: *const Region = params as *const Region;
+  let region: &Region = unsafe { &*region as &_ };
+  env.val + 18 + region.len
 }
